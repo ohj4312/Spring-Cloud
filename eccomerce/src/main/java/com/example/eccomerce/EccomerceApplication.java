336c45bfe6dbf5c47fd7728b2e793223a0762219
ohj4312 <ohj4312@naver.com>
2022-01-23 14:36:40 +0900

    eureka server and client example

---
+++ eccomerce/src/main/java/com/example/eccomerce/EccomerceApplication.java
@@ -0,0 +1,15 @@
+package com.example.eccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EccomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceApplication.class, args);
+	}
+
+}
